ab8e6f66779fd7290cc77ae88356b10a0e9f8374
sjsh1623 <sjsh1623@naver.com>
2019-09-11 21:22:21 +0900

    Add files via upload

---
+++ Summation of primes.cpp
@@ -0,0 +1,43 @@
+﻿/*
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+bool isprime(int i){
+    for (int k = 2; k <= sqrt(i); k++)
+    {
+        if (i % k == 0) {
+            return false;
+            break;
+        }
+        }
+    return true;
+    }
+
+
+long add_prime(int j){
+    long store = 0;
+    for (int i = 2; i < j; i++)
+    {
+        if (isprime(i) == true)
+        {
+            store = store + i;
+        }
+
+    }
+    return store;
+}
+
+int main()
+{
+    long i = 2000000; //limit
+    long result = 0;
+
+    result = add_prime(i);
+    printf("Sum of all the primes below %d is %d", i, result);
+}
+
